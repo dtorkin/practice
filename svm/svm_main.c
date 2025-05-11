d5cc5d36a2a9b4c6573036e5980171f3a4091b55
dtorkin <danilatorkin2003@gmail.com>
2025-05-11 18:03:11 +0300

    Update svm_main.c

--- svm/svm_main.c
+++ svm/svm_main.c
@@ -295,12 +295,12 @@ int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
     for (int i = 0; i < MAX_SVM_INSTANCES; ++i) {
         initialize_svm_instance(&svm_instances[i], i);
 		printf("DEBUG SVM ID %d: simulate_control_failure=%d, disconnect_after=%d, simulate_timeout=%d, send_warning=%d, tks=%u\n",
-       id, // или instance->id
-       config.svm_settings[id].simulate_control_failure, // или instance->simulate_control_failure
-       config.svm_settings[id].disconnect_after_messages,
-       config.svm_settings[id].simulate_response_timeout,
-       config.svm_settings[id].send_warning_on_confirm,
-       config.svm_settings[id].warning_tks);
+       instance->id, // или instance->id
+       config.svm_settings[instance->id].simulate_control_failure, // или instance->simulate_control_failure
+       config.svm_settings[instance->id].disconnect_after_messages,
+       config.svm_settings[instance->id].simulate_response_timeout,
+       config.svm_settings[instance->id].send_warning_on_confirm,
+       config.svm_settings[instance->id].warning_tks);
         if (pthread_mutex_init(&svm_instances[i].instance_mutex, NULL) != 0) {
             perror("Failed to initialize instance mutex");
             for (int j = 0; j < i; ++j) pthread_mutex_destroy(&svm_instances[j].instance_mutex);
